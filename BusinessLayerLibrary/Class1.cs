10ffb471b99ed26dca132c51ddf5e75e410dd8e3
abhaysurya10 <abhaysurya10@gmail.com>
2023-11-29 13:39:56 +0530

    Add project files.

---
+++ BusinessLayerLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayerLibrary
+{
+    public class CreateMyDatabase
+    {
+        public CreateMyDatabase()
+        {
+            EmsContext emsContext = new EmsContext();
+        }
+    }
+}
